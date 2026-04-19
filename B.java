45e0b64b0eeba73bb5aa9f2cab20609150479557
Rudra Lathiya <rudralathiya224@gmail.com>
2026-04-19 21:29:34 +0530

    Delete B.java

--- B.java
+++
@@ -1,9 +0,0 @@
-Practical 13 - Multi-Package Access Specifier Demo
-===================================================
-Run all commands from inside the Practical13_Packages/ folder:
-
-  javac apack/A.java
-  javac -cp . bpack/B.java
-  javac -cp . cpack/C.java
-  javac -cp . dpack/ProtectedDemo.java
-  java -cp . dpack.ProtectedDemo
